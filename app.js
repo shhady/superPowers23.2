47edff216b82b13c6ad75db8e1467981e05d5039
shhady <shhadyse@gmail.com>
2022-04-27 22:51:21 +0300

    superPowers23.2

---
+++ app.js
@@ -0,0 +1,15 @@
+// Your Task: Edit the storm object. Invoke the printSuperPower function using
+// the storm object as the context of the function.
+// So your output should be: my super power is flying.
+// Well, Storm also controls the weather, so, whatever you choose!
+// You cannot change the printSuperPower function
+// Submit the file to Hive.
+const storm = {
+  superPower: "diving",
+};
+function printSuperPower() {
+  console.log("my superpower is " + this.superPower);
+}
+
+storm.printSuperPower = printSuperPower;
+storm.printSuperPower();
